d29c1c84233fca1b7c9849107270af530d648e2e
ExoticSolutions <1exoticeditz@gmail.com>
2025-10-20 21:37:37 -0400

    updates

---
+++ command_pattern/Client.java
@@ -0,0 +1,13 @@
+// Client.java
+package labs.command_pattern;
+
+
+public class Client {
+    public static void main(String[] args) {
+        Receiver receiver = new Receiver();
+        Command command = new ConcreteCommand(receiver);
+        Invoker invoker = new Invoker();
+        invoker.setCommand(command);
+        invoker.invoke();
+    }
+}
---
+++ command_pattern/Receiver.java
@@ -0,0 +1,6 @@
+package labs.command_pattern;
+public class Receiver {
+    public void action() {
+        System.out.println("Receiver: Performing action...");
+    }
+}
